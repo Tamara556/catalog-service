9706f8d4b75f07b6964403d97b7ba70c4cb1bea8
Tamara Murazyan <murazyan.tamara5566.com>
2025-03-25 15:26:08 +0400

    Add data with flyway

--- src/main/java/com/example/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/example/catalogservice/CatalogServiceApplication.java
@@ -1,14 +1,15 @@
 package com.example.catalogservice;
 
-import com.example.catalogservice.config.PolarProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-@EnableConfigurationProperties(PolarProperties.class)
+@ConfigurationPropertiesScan
 public class CatalogServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
+
 }
--- src/main/java/com/example/catalogservice/domain/BookAlreadyExistsException.java
+++ src/main/java/com/example/catalogservice/domain/BookAlreadyExistsException.java
@@ -1,6 +1,5 @@
 package com.example.catalogservice.domain;
 
-
 public class BookAlreadyExistsException extends RuntimeException {
 
     public BookAlreadyExistsException(String isbn) {
